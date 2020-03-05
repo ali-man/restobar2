08a0e19fb90b031c4aa8c48b1872199b68d554b2
Ali-man <aliman.fsd@gmail.com>
2020-03-05 21:02:29 +0500

    Redesign

--- apps/general/views.py
+++ apps/general/views.py
@@ -6,4 +6,4 @@ from apps.products.models import Category
 
 def home_page(request):
     slider = Slider.objects.all()
-    return render(request, 'home.html', locals())
+    return render(request, 'home/index.html', locals())
